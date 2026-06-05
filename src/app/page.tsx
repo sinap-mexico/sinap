ee784bacec6baea4dfe586a8817b93af5694a1ac
Z User <z@container>
2026-06-05 09:58:57 +0000

    0b1ddb77-9356-4f14-a878-d12e99b1fff7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import SinapDashboard from './dashboard/page'
+import { redirect } from 'next/navigation'
 
 export default function HomePage() {
-  return <SinapDashboard />
+  redirect('/login')
 }
